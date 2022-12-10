b2c6eff5566a7395e212454baf56a3172f5130fe
Aydin-sarrafi-khosroshahi <101389534+Aydin-sarrafi-khosroshahi@users.noreply.github.com>
2022-12-11 00:32:21 +0330

    2th day

--- assets/js/script.js
+++ assets/js/script.js
@@ -132,65 +132,129 @@ const AllTeams = [
 
 
 $('#submit-btn').click(function () {
+
     var inputNum = $('#countries-number').val();
     if( inputNum < 0 ) {
         alert("this cant be negative");
     }
+
     else if( inputNum == 0 ) {
         alert("this cant be 0");
     }
-    else if ( inputNum > 20 ) {
+
+    else if( inputNum > 20 ) {
         alert("there are 20 teams");
     }
-    else if( inputNum % 4 == 0 ) {
-
-        // var randomIndex = Math.floor( Math.random() * AllTeams.length ); 
-
-        var randomIndex = [];
-        var teamsGroup = [];
-        var groups = [];
-
-        for( let i = 0 ; i <= inputNum-1 ; i++ ) {
-
-            var random = Math.floor( Math.random() * AllTeams.length );
-            randomIndex.push( random ); 
-            
-            // AllTeams.splice( random , 1 );
-
-        }
-
-        for( let j = 0 ; j <= randomIndex.length-1 ; j++ ) {
-
-            for( let z = 0 ; z <= AllTeams.length-1 ; z++) {
-                
-                if( randomIndex[j] == AllTeams[z].id ) {
-
-                    var obj = {};
-                    obj["name"] = AllTeams[z].name;
-                    obj["id"] = AllTeams[z].id;
-
-                    teamsGroup.push(obj);
-
-                }// end if
-
-            }// end for(2)
-
-
-        }// end for(1)
-        
-        const chunkSize = 4;
-        for ( let i = 0; i < teamsGroup.length; i += chunkSize ) {
-
-            groups.push( teamsGroup.slice(i, i + chunkSize) );
-            
-        }
-        console.log(groups);
-
-    }// end main if
 
     else {
-        alert("The number must be divisible by 4");
+
+        // var nums = [1,2,3,4,5,6,7,8,9,10],
+        // ranNums = [],
+        // i = nums.length,
+        // j = 0;
+
+        // while (i--) {
+        //     j = Math.floor(Math.random() * (i+1));
+        //     ranNums.push(nums[j]);
+        //     nums.splice(j,1);
+        // }
+
+        var ranNumArr = [];
+        var TeamsLength = AllTeams.length;
+        var j = 0;
+
+        while( inputNum-- ) {
+
+            j = Math.floor( Math.random() * ( inputNum + 1 ) );
+            ranNumArr.push( AllTeams[j] );
+            AllTeams.splice( j , 1 );
+
+        }
+
+        for( let i = 0 ; i <= ranNumArr.length ; i++ ) {
+
+            console.log( ranNumArr[i].name );
+
+        }
+
+        // for( let i = 0 ; i <= inputNum-1 ; i++ ) {
+
+        //     var randomNumber = Math.floor( Math.random() * AllTeams.length );
+
+        //     if( !randomTeamsIndex.includes( randomNumber ) ) {
+
+        //         randomTeamsIndex[i] = randomNumber;
+
+        //     }
+
+
+        // }
+
+
+        // cheack that if we have reputition complete the 4 teams
+        // if( randomTeamsIndex.length < inputNum ) {
+
+        //     while( randomTeamsIndex.length < inputNum ) {
+
+        //         randomTeamsIndex.push( randomNumber );
+
+        //     }
+
+        // }
+
+        //console.log(AllTeams);
+        //console.log(randomTeamsIndex);
+        
     }
+    // else if( inputNum % 4 == 0 ) {
+
+    //     // var randomIndex = Math.floor( Math.random() * AllTeams.length ); 
+
+    //     var randomIndex = [];
+    //     var teamsGroup = [];
+    //     var groups = [];
+
+    //     for( let i = 0 ; i <= inputNum-1 ; i++ ) {
+
+    //         var random = Math.floor( Math.random() * AllTeams.length );
+    //         randomIndex.push( random ); 
+            
+    //         // AllTeams.splice( random , 1 );
+
+    //     }
+
+    //     for( let j = 0 ; j <= randomIndex.length-1 ; j++ ) {
+
+    //         for( let z = 0 ; z <= AllTeams.length-1 ; z++) {
+                
+    //             if( randomIndex[j] == AllTeams[z].id ) {
+
+    //                 var obj = {};
+    //                 obj["name"] = AllTeams[z].name;
+    //                 obj["id"] = AllTeams[z].id;
+
+    //                 teamsGroup.push(obj);
+
+    //             }// end if
+
+    //         }// end for(2)
+
+
+    //     }// end for(1)
+        
+    //     const chunkSize = 4;
+    //     for ( let i = 0; i < teamsGroup.length; i += chunkSize ) {
+
+    //         groups.push( teamsGroup.slice(i, i + chunkSize) );
+            
+    //     }
+    //     console.log(groups);
+
+    // }// end main if
+
+    // else {
+    //     alert("The number must be divisible by 4");
+    // }
 })
 
 
